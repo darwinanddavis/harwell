443cd0bb4d1ee206eac4712dd424143d2c598ca2
darwinanddavis <matthew.malishev@gmail.com>
2019-02-01 16:06:40 -0500

    init

---
+++ harwell_gift.R
@@ -0,0 +1,23 @@
+# harwell reward program
+
+# 1. write function to play file every hour
+# (most likely from cron or bash hereafter (https://superuser.com/questions/243893/how-to-make-run-cron-on-osx-10-6-2))
+# 2. stop sound file when key is pressed (login to comp)
+# 3. get time elapsed since play started and any key pressed (+ perhaps random wait time added)
+# 4. input time elapsed into vector to make distribution (must be positive integer)
+# 5. either a) sample from dist randomly or b) increase frequency of play time based on time elapsed (step 6)
+# 6. ### to increase play time frequency. sample number from lower tail end of above dist. 
+# if median of dist deviates further from 0 (i.e. lower tail of dist become longer to indicate more time has passed since pressing a key), increase time to replay sound file. this needs to log incremental differences, not absolute, otherwise tail will always be longer.   
+# if median of dist remains the same/approaches 0 (indicating quicker response time), decrease time to play sound file.
+# if too much time elapses (no one in office/good behaviour reward), turn off sound file
+# 7. the above reward system incentivises listening to the song in full for longer time periods. 
+# e.g. set.seed(10);plot(density(sample(10)));abline(v=0);set.seed(50);plot(density(sample(50)));abline(v=0)
+1/200
+
+
+smashmouth_hour <- substr(format(Sys.time(), "%X")[1],1,2)
+if(Sys.time()==smashmouth_hour){print("It's SMASH TIME")}
+as.POSIXct(smashmouth_hour)
+
+format(Sys.time(), "%a %b %d %X %Y")
+substr(format(Sys.time(), "%X")[1],1,2)
---
+++ snab_wavs.R
@@ -0,0 +1,64 @@
+# src http://www.pacdv.com/sounds/voices-4.html
+packages <- c("RCurl","beepr")
+if(require(packages)){
+  install.packages(packages,dependencies = T)
+}
+ppp <- lapply(packages,require,character.only=T)
+if(any(ppp==F)){cbind(packages,ppp);cat("\n\n\n ---> Check packages are loaded properly <--- \n\n\n")}
+
+# smashmouth 
+# https://www.youtube.com/watch?v=xRbNB5VopMM&index=1&list=RDahXQ7e8w7mQ
+# https://youtu.be/rlYys58hsCU?list=RDahXQ7e8w7mQ
+# https://www.youtube.com/watch?time_continue=24&v=ahXQ7e8w7mQ # windows XP ver
+
+# ******* back end code to call as snab_wavs.R *********** 
+# steps
+# 1. find wav files online
+# 2. save urls to list in R
+# 3. Write a function to read list and plat sound file (w vol and repeat options)
+# 4. save function to online repo
+# 5. pull online file into R
+# 6. embed code in R to play sound file 
+
+snab_wav <- function(sound,vol,reps){
+  snab_wavs_list <- list( # 1 = random default pck sound 
+    "http://www.pacdv.com/sounds/voices/oh-my-god-1.wav", # omg
+    "http://www.pacdv.com/sounds/voices/okay-2.wav", # OK
+    "http://www.pacdv.com/sounds/voices/mmm-1.wav", # mmm
+    "http://www.pacdv.com/sounds/voices/open-the-goddamn-door.wav", # open the door
+    "http://www.pacdv.com/sounds/voices/rrrou-1.wav", # rwar
+    "http://www.pacdv.com/sounds/voices/rrrou-2.wav", # rwar2
+    "http://www.pacdv.com/sounds/voices/shh-shh.wav", # shh
+    "http://www.pacdv.com/sounds/voices/thank-god-its-friday.wav", # friyay
+    "http://www.pacdv.com/sounds/voices/that-feels-really-powerful.wav", # powerful
+    "http://www.pacdv.com/sounds/voices/thats-okay-1.wav", # that's OK
+    "http://www.pacdv.com/sounds/voices/this-is-ridiculous.wav", # ridiculous 
+    "http://www.pacdv.com/sounds/voices/who-are-you.wav", # who are you
+    "http://www.pacdv.com/sounds/voices/have-a-good-weekend.wav" # weekend
+  )
+  # sound <- length(snab_wavs_list)
+  # vol <- 5 # !!! 5 = loud! 
+  # reps <- 1 # no. of times to repeat sound
+  if(sound==0){sound <- sample(length(snab_wavs_list),1)}
+  if(sound==1){
+    play_file <- replicate(reps,{beep(rep_len(sample(11,1),1))})
+    }else{
+    play_file <- replicate(reps,{beep(rep_len(snab_wavs_list[[sound]],1))})
+    }
+} # end function  
+
+# pull snab wav function
+script <- getURL("snab_wavs.R", ssl.verifypeer = FALSE)
+play_sound <- eval(parse(text = script))
+
+# now execute the above file 
+cat("\n\n",rep("-",30),"\nEnter number to represent sound file ( up to ",length(snab_wavs_list),") or '0' to play a random sound
+    \nEnter the volume level (5 is average vol)
+    \nEnter how many times you want the sound repeated\n"
+    ,rep("-",30),
+    "\n\n")
+
+# run below function 
+snab_wav(6,5,2) 
+
+
